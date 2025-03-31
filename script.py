77a92f45eef4f692280556d3e1a0505b356fe850
Alopsis <105967355+Alopsis@users.noreply.github.com>
2025-03-31 14:54:24 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import json
+import base64
+import re
 from datetime import datetime, timedelta, UTC, timezone
 import os
+import subprocess
 import requests
 import base64
 
@@ -8,18 +11,13 @@ import base64
 # Méthode avec le regex pour le HTTP HEADER #
 #############################################
 
-# Variables
-query = ""
-entityName = ""
-api_key = ""
-date_before = 3 
-sample_string_bytes = query.encode("ascii")
+# On récupere les 2 premieres pages 
+entity_name = "{ENTITY_NAME_HERE}"
+api_key = "{API_KEY_HERE}"
+sample_string_bytes = f"http.header='{entity_name}'".encode("ascii")
+
 request = base64.b64encode(sample_string_bytes)
-date_limite = datetime.now(timezone.utc) - timedelta(days=date_before)
-
-
-# Recherche sur les 3 premières pages
-for i in range(1, 4):
+for i in range(1, 2):
 
     cmd = f"""curl -X POST "https://api.zoomeye.ai/v2/search" \
     -H "API-KEY: {api_key}" \
@@ -29,14 +27,19 @@ for i in range(1, 4):
     os.system(cmd)
 
 
-# Traitement des résultats
+date_before = 3 
+date_limite = datetime.now(timezone.utc) - timedelta(days=date_before)
+
+
+
 directory = "./result"
+
 for filename in os.listdir(directory):
     if filename.endswith(".json"):
         file_path = os.path.join(directory, filename)
         with open(file_path, "r") as file:
             content = json.load(file)
-            pattern = r'(https?://[^\s]+'+entityName+'\.[^\s]+)'
+            pattern = r'(https?://[^\s]+geodis\.[^\s]+)'
             data = content.get("data")
             if data is not None:
                 for item in content["data"]:
@@ -44,11 +47,10 @@ for filename in os.listdir(directory):
                         date_obj = datetime.strptime(item["update_time"], "%Y-%m-%dT%H:%M:%S.%f")
                     except ValueError:
                         date_obj = datetime.strptime(item["update_time"], "%Y-%m-%dT%H:%M:%S")
+                    
                     date_obj = date_obj.replace(tzinfo=timezone.utc) 
                     if date_obj < date_limite:
 
                         urls = re.findall(pattern, item["header"])
                         for url in urls:
                             print(url)
-
-
